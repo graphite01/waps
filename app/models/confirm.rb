ac38f379925e8ecfb305603eaf9cee818099a8ac
graphite01 <kirito10082279@gmail.com>
2022-10-20 16:47:12 +0900

    confirmモデル作成

--- app/models/confirm.rb
+++ app/models/confirm.rb
@@ -1,8 +1,8 @@
-class Hope < ApplicationRecord
+class Confirm < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :work_status
   belongs_to :user
-  has_one :confirm
+  belongs_to :hope
 
   with_options numericality: { other_than: 1, message: 'を入力してください' } do
     validates :work_status_id
@@ -12,12 +12,11 @@ class Hope < ApplicationRecord
   validate :start_check
 
   def start_end_check
-    if work_status_id == 2
-      errors.add(:end_time, 'は出勤時間より遅い時間を選択してください') if start_time >= end_time
-    end
+    errors.add(:end_time, 'は出勤時間より遅い時間を選択してください') if start_time >= end_time
   end
 
   def start_check
     errors.add(:start_time, 'は現在の日時より遅い時間を選択してください') if start_time < Time.now
   end
+
 end
