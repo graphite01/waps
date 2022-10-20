6f76e803b1adc04b234b88ad82d5be3ec6450f6c
graphite01 <kirito10082279@gmail.com>
2022-10-20 17:29:38 +0900

    管理者だけがシフト作成画面に遷移できる

--- app/controllers/confirms_controller.rb
+++ app/controllers/confirms_controller.rb
@@ -1,14 +1,23 @@
 class ConfirmsController < ApplicationController
+  before_action :manager_only, except: [:index]
+
   def index
     #@confirms = Confirm.all 作成後に切り替え
     @confirms = Confirm.all
   end
   def new
+    @hopes = Hope.all
     @confirms = Confirm.all
     @hope = Hope.new
   end
   def edit
     @confirm = @confirm_date.all
     @confirm = Confirm.find(params[:id])
+  end
+
+  def manager_only
+    if current_user.account_type_id != 3
+      redirect_to root_path
+    end
   end
 end
