8d8486d5c3c57a4fb61c1946ed2aa39d49c2801c
graphite01 <kirito10082279@gmail.com>
2022-10-26 15:20:18 +0900

    ユーザー情報編集機能&ユーザー一覧機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,31 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
     @confirm = @user.confirms
   end
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to user_path(current_user.id)
+      sign_in(current_user, :bypass => true) 
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :position, :account_type_id, :password)
+  end
+
 end
