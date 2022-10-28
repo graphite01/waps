e19e88432153a5e3273d8afb08c1d764bc5e9307
graphite01 <kirito10082279@gmail.com>
2022-10-28 13:16:09 +0900

    commentモデルテストコード実施

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2022_10_26_062942) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "content"
+    t.string "content", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
   factory :comment do
+    content { Faker::Lorem.sentence }
+    user_id { Faker::Number.between(from: 2, to: 4) }
+    association :user
   end
 end
